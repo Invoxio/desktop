ef94e4b6a6eca7d22541100f2133b46263a8c62f
Tim Meade <tim@invoxio.com>
2019-07-21 16:55:27 -0400

    Cleared issues

--- app/containers/Pages/Home/index.js
+++ app/containers/Pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
@@ -24,7 +24,7 @@ export class Home extends React.PureComponent {
 }
 
 Home.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  // dispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
